3305b24494ac8337487412782b98606ebb73a81b
KristyMoussally <2242744@cmontmorency.qc.ca>
2024-03-02 22:17:15 -0500

    P1.3c swiper App

--- moussally_sing_P01/js/app_script.js
+++
@@ -1,6 +0,0 @@
-const swiper = new Swiper(".swiper", {
-  grabCursor: true,
-  slidesPerView: 1.49,
-  spaceBetween: 2,
-  loop: true,
-});
---
+++ moussally_sing_P01/js/script.js
@@ -0,0 +1,11 @@
+const swiperButtonsList = new Swiper(".swiper-filters", {
+  grabCursor: true,
+  slidesPerView: 1.6,
+  spaceBetween: 1,
+});
+
+const swiperArtworksVedette = new Swiper(".swiper-featured-artworks", {
+  grabCursor: true,
+  slidesPerView: 1.95,
+  spaceBetween: 1,
+});
