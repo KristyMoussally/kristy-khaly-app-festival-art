2f7ba0276b9a720888d78dabef08c5f03640669c
KristyMoussally <2242744@cmontmorency.qc.ca>
2024-03-11 08:29:29 -0400

    App a corriger encore

--- moussally_sing_P01/js/script.js
+++ moussally_sing_P01/js/script.js
@@ -1,11 +1,17 @@
 const swiperButtonsList = new Swiper(".swiper-filters", {
   grabCursor: true,
-  slidesPerView: 1.7,
+  slidesPerView: 2.6,
   spaceBetween: 1,
 });
 
 const swiperArtworksVedette = new Swiper(".swiper-featured-artworks", {
   grabCursor: true,
-  slidesPerView: 1.95,
+  slidesPerView: 2,
   spaceBetween: 1,
+});
+
+const btnLike = document.querySelector("#like");
+
+btnLike.addEventListener("click", function () {
+  btnLike.classlist.replace("bi bi-balloon-heart", "active");
 });
