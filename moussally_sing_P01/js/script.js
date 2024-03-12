5e5c2801466a4528e88955c6a5d9b85ffad0460f
KristyMoussally <2242744@cmontmorency.qc.ca>
2024-03-12 14:05:47 -0400

    Correction partie App

--- moussally_sing_P01/js/script.js
+++ moussally_sing_P01/js/script.js
@@ -1,3 +1,11 @@
+let btnLike = document.getElementById("like");
+
+if (btnLike !== null) {
+  btnLike.addEventListener("click", () => {
+    btnLike.classList.toggle("active");
+  });
+}
+
 const swiperButtonsList = new Swiper(".swiper-filters", {
   grabCursor: true,
   slidesPerView: 2.6,
@@ -8,10 +16,4 @@ const swiperArtworksVedette = new Swiper(".swiper-featured-artworks", {
   grabCursor: true,
   slidesPerView: 2,
   spaceBetween: 1,
-});
-
-const btnLike = document.querySelector("#like");
-
-btnLike.addEventListener("click", function () {
-  btnLike.classlist.replace("bi bi-balloon-heart", "active");
 });
