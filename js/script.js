4ebe91c460a87d917179ab969c708d02e1ab96dd
Khatymiss <khalytia@hotmail.ca>
2024-03-14 10:14:25 -0400

    swiper_final

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 var swiper = new Swiper(".swiper-screenshots", {
+  slidesPerView: 1,
   loop: true,
+
+  breakpoints: {
+    1200: {
+     slidesPerView: 3,
+     slidesPerGroup: 1,
+     spaceBetween: 25,
+     centeredSlides: true,
+   centeredSlidesBounds: true
+   },
+ },
+
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
